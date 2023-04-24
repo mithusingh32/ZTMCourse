167336a2e6023fa658be7d67ca7b0ea4f9436367
Sukhmeet Khalar <sukhmeet@sukhmeetkhalar.com>
2023-04-24 06:55:48 -0400

    completed impl activity

--- Rust/activities/src/bin/a12.rs
+++ Rust/activities/src/bin/a12.rs
@@ -6,8 +6,45 @@
 //
 // Notes:
 // * Use a struct to encapsulate the box characteristics
+
+struct Box {
+    width: i32,
+    height: i32,
+    weight: i32,
+    color: Color,
+}
+
 // * Use an enum for the box color
+#[derive(Debug)]
+enum Color {
+    Green,
+    Brown,
+    Black,
+    White,
+}
+
 // * Implement functionality on the box struct to create a new box
+impl Box {
+    fn create_box(width: i32, height: i32, weight: i32, color: Color) -> Self {
+        Self {
+            width,
+            height,
+            weight,
+            color,
+        }
+    }
+
+    fn print_info(&self) {
+        println!(
+            "The box is {} wide, {}, tall, weighs {}, and is the color {:?}",
+            self.width, self.height, self.weight, self.color
+        )
+    }
+}
+
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+fn main() {
+    let a_box = Box::create_box(10, 10, 100, Color::White);
+    a_box.print_info();
+}
