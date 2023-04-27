51679b6d877ce73babdb721b446cc9c8e49a3a65
Sukhmeet Khalar <sukhmeet@sukhmeetkhalar.com>
2023-04-27 14:57:31 -0400

    strings completed

--- Rust/activities/src/bin/a14.rs
+++ Rust/activities/src/bin/a14.rs
@@ -4,11 +4,59 @@
 // * Print out the name and favorite colors of people aged 10 and under
 //
 // Notes:
-// * Use a struct for a persons age, name, and favorite color
+// * Use a struct for a persons age, name, and favorite colori
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
 // * The color and name should be stored as a String
 // * Create and store at least 3 people in a vector
 // * Iterate through the vector using a for..in loop
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+fn main() {
+    let people = vec![
+        Person {
+            age: 10,
+            name: String::from("Person1"),
+            fav_color: String::from("Red"),
+        },
+        Person {
+            age: 20,
+            name: String::from("Person2"),
+            fav_color: String::from("Green"),
+        },
+        Person {
+            age: 8,
+            name: String::from("Person3"),
+            fav_color: String::from("Yellow"),
+        },
+        Person {
+            age: 12,
+            name: String::from("Person4"),
+            fav_color: String::from("Brown"),
+        },
+        Person {
+            age: 89,
+            name: String::from("Person5"),
+            fav_color: String::from("Green"),
+        },
+        Person {
+            age: 4,
+            name: String::from("Person6"),
+            fav_color: String::from("Purple"),
+        },
+    ];
+
+    for person in &people {
+        if person.age >= 10 {
+            println!(
+                "{} is {} years old who like {}",
+                person.name, person.age, person.fav_color
+            );
+        }
+    }
+}
--- Rust/notes/src/bin/string.rs
+++ Rust/notes/src/bin/string.rs
@@ -6,6 +6,11 @@
 // If you need to use a string in a struct, it must be String and not a slice
 // When passing string around, use the &str and not the String
 
+//Struct can only use String and not the sliced string
+struct Example {
+    string: String, // string: &str is not allows
+}
+
 //Passing String to a function
 fn print_str(data: &str) {
     // We pass in the borrowed string slice to the function
@@ -13,8 +18,16 @@ fn print_str(data: &str) {
 }
 
 fn main() {
+    print_str("this is a borrowed string"); // This is a borrowed string so the function owns the
+                                            // string
     let owned_string = "owned string".to_owned();
     let another_owned = String::from("another owned string");
     print_str(&owned_string);
     print_str(&another_owned);
+
+    let ex = Example {
+        string: String::from("string in a struct"),
+    };
+
+    print_str(&ex.string);
 }
